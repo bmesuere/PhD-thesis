99eb94b763005dedd59358441da9a1aa06b09d17
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-07-19 17:38:24 +0200

    update create script

--- create-book.sh
+++ create-book.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
 
+rm -rf output
+mkdir output
+cp -r styles output/
+
 # -f markdown: use markdown as input
 # -t html: use html as output
-# -s: create stand alone file
 # -o: output filename
-pandoc test.md -f markdown -t html -s -o test.html
+# -s: create stand alone file
+# -S: use smart quotes in output
+# --toc: include table of content
+# -N: number sections
+# -css: add style sheet
+pandoc test.md \
+    -f markdown \
+    -t html5 \
+    -o output/book.html \
+    -s \
+    -S \
+    --toc \
+    -N \
+    --css='styles/html.css'
+
+#prince output/book.html -o output/book.pdf
