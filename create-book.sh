0ef0dea3bbe01afebd1ccd640f8f347a48ab5126
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-07-20 10:38:23 +0200

    add citations

--- create-book.sh
+++ create-book.sh
@@ -12,14 +12,16 @@ cp -r styles output/
 # --toc: include table of content
 # -N: number sections
 # -css: add style sheet
-pandoc test.md \
+pandoc chapter2.md \
     -f markdown \
     -t html5 \
     -o output/book.html \
     -s \
     -S \
-    --toc \
     -N \
-    --css='styles/html.css'
+    --css='styles/html.css' \
+    --css='styles/chapter2.css' \
+    --bibliography='library.bib'
+#    --toc \
 
 #prince output/book.html -o output/book.pdf
