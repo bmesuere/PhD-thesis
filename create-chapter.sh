1dc679b257a11cf16cbdd01a809b7d8e040b9e4f
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-07-30 20:54:18 +0200

    add chapter generation script

---
+++ create-chapter.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+rm -rf output
+mkdir output
+cp -r styles output/
+cp -r images output/
+cp hyphenator.js output/
+
+version='version x'
+
+pandoc chapter2.md \
+    -f markdown \
+    -t html5 \
+    -o output/web.html \
+    -s \
+    -S \
+    -N \
+    --metadata="date:$version" \
+    --number-sections \
+    --section-divs \
+    --chapters \
+    -M chapters \
+    --filter='/Users/bart/.cabal/bin/pandoc-crossref' \
+    --css='styles/web.css' \
+    --css='styles/chapter2.css' \
+    --bibliography='library.bib' \
+    --include-in-header='head.html' \
