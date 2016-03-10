df35535b066c140d22659dc3af8eca4b04e75f08
Bart Mesuere <Bart.Mesuere@UGent.be>
2016-03-10 17:53:28 +0100

    proofread chapter 7

--- create-book.sh
+++ create-book.sh
@@ -10,22 +10,22 @@ version='version '
 version+=`date +%Y.%m.%d-`
 version+=`git rev-list HEAD --count`
 
-# pandoc chapter0.md chapter1.md chapter2.md chapter3.md chapter4.md chapter5.md chapter6.md chapter7.md chapterz.md \
-#     -f markdown \
-#     -t html5 \
-#     -o output/web.html \
-#     -s \
-#     -S \
-#     -N \
-#     --metadata="date:$version" \
-#     --number-sections \
-#     --section-divs \
-#     --chapters \
-#     -M chapters \
-#     --filter='/Users/bart/.cabal/bin/pandoc-crossref' \
-#     --css='styles/web.css' \
-#     --bibliography='library.bib' \
-#     --include-in-header='head.html' \
+pandoc chapter0.md chapter1.md chapter2.md chapter3.md chapter4.md chapter5.md chapter6.md chapter7.md chapterz.md \
+    -f markdown \
+    -t html5 \
+    -o output/web.html \
+    -s \
+    -S \
+    -N \
+    --metadata="date:$version" \
+    --number-sections \
+    --section-divs \
+    --chapters \
+    -M chapters \
+    --filter='/Users/bart/.cabal/bin/pandoc-crossref' \
+    --css='styles/web.css' \
+    --bibliography='library.bib' \
+    --include-in-header='head.html' \
 
 # -f markdown: use markdown as input
 # -t html: use html as output
